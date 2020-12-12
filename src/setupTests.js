232cb1fd95119cc7737bd7a26e2b87b761d59122
Dzmitry Lahunouski <dzmitry_lahunouski@epam.com>
2020-12-12 15:38:11 +0300

    setup test & folder structure

--- src/setupTests.js
+++ src/setupTests.js
@@ -3,3 +3,8 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+import '@testing-library/react-hooks';
+import { render } from '@testing-library/react';
+import '@testing-library/user-event';
+
+global.render = render;
